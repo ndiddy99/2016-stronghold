d1554ce3f881a00d4bea82248d6b0fcf374bfc57
Alex Taber <alex@ahsraiderreview.com>
2016-01-29 11:43:29 -0500

    Made some changes to HumanInput

--- Stronghold/src/org/usfirst/frc/team2537/robot/input/HumanInput.java
+++ Stronghold/src/org/usfirst/frc/team2537/robot/input/HumanInput.java
@@ -7,14 +7,6 @@ import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team2537.robot.input.xBoxButtons;
 
 public class HumanInput {
-	public static final int
-		XBOX_LEFT_STICK_X_AXIS = 0,
-		XBOX_LEFT_STICK_Y_AXIS = 1,
-		XBOX_LEFT_TRIGGER_AXIS = 2,
-		XBOX_RIGHT_TRIGGER_AXIS = 3,
-		XBOX_RIGHT_STICK_X_AXIS = 4,
-		XBOX_RIGHT_STICK_Y_AXIS = 5,
-		XBOX_DIRECTIONAL_PAD = 6;
 	
 	public static Joystick xboxController = new Joystick(Ports.XBOX);
 	
@@ -29,5 +21,21 @@ public class HumanInput {
 	
 	public static double getXboxAxis(Joystick j, int i) {
 		return j.getRawAxis(i);
+	}
+	
+	public static boolean getLeftTriggerState() {
+		if (getXboxAxis(xboxController, xBoxButtons.XBOX_TRIGGERS) > 0.5) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static boolean getRightTriggerState() {
+		if (getXboxAxis(xboxController, xBoxButtons.XBOX_TRIGGERS) < -0.5) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
