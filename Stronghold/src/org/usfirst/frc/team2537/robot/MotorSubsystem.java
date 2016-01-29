47bed7e84ab1f066200d05fe0e0e4b51cd2b2df2
karan <karan@Karan-HPFuture>
2016-01-29 17:47:26 -0500

    Enabled the Cims Soft Limits

--- Stronghold/src/org/usfirst/frc/team2537/robot/MotorSubsystem.java
+++ Stronghold/src/org/usfirst/frc/team2537/robot/MotorSubsystem.java
@@ -28,18 +28,18 @@ public class MotorSubsystem extends Subsystem {
 		climberB.reverseSensor(false);
 		climberF.setPosition(0);
 		climberB.setPosition(0);
+		climberF.enableForwardSoftLimit(true);
+		climberB.enableForwardSoftLimit(true);
 		climberF.setForwardSoftLimit(+15);//Replace with the max rotations that the tape measure can go forward
-		climberB.setForwardSoftLimit(-15);//Replace with the max rotations that the tape measure can go forward
+		climberB.setForwardSoftLimit(+15);//Replace with the max rotations that the tape measure can go forward
 	}
 	
 	public static void set(CANTalon t, double speed) {
 		t.set(speed);
 	}
 	public static void getEncValue(CANTalon t){
-		t.getEncPosition();
-		
+		t.getEncPosition();	
 	}
-
 	@Override
 	protected void initDefaultCommand() {
 		TalonCommand tc = new TalonCommand();
