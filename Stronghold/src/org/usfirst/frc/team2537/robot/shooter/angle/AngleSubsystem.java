26773b9198806c291b811840e7261d5fc20fa5fe
srgu2537 <matthewschweiss@verizon.net>
2016-01-30 17:49:12 -0500

    Commented Code

--- Stronghold/src/org/usfirst/frc/team2537/robot/shooter/angle/AngleSubsystem.java
+++ Stronghold/src/org/usfirst/frc/team2537/robot/shooter/angle/AngleSubsystem.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team2537.robot.shooter.angle;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.CANTalon;
-
 import java.util.HashMap;
 
 //import org.usfirst.frc.team2537.robot.input.HumanInput;
@@ -13,19 +12,23 @@ import org.usfirst.frc.team2537.robot.input.SensorListener;
 
 /**
  * @author Matthew Schweiss
+ * 
+ * This is the base class for anything adjusting the angle of the 
+ * shooter.
  *
  */
 public class AngleSubsystem extends Subsystem implements SensorListener {
 	//Limit Switches
 	// TODO determine default states of limit switches
-		private static final boolean FWD_LIMIT_SWITCH_NORMALLY_OPEN = true;
-		private static final boolean REV_LIMIT_SWITCH_NORMALLY_OPEN = true;
+	private static final boolean FWD_LIMIT_SWITCH_NORMALLY_OPEN = true;
+	private static final boolean REV_LIMIT_SWITCH_NORMALLY_OPEN = true;
 	
 	//The angle limits.
 	public static final double MAX_ANGLE = 80;//degrees (ball park, not right)
 	public static final double MIN_ANGLE = -40;//degrees(ball park, not right)
+	
+	//Varibles
 	private static double currentAngle = 0;
-	//All of the motors.
 	private final CANTalon angleTalon;
 	
 	public AngleSubsystem() {
@@ -36,7 +39,6 @@ public class AngleSubsystem extends Subsystem implements SensorListener {
 		angleTalon.ConfigFwdLimitSwitchNormallyOpen(FWD_LIMIT_SWITCH_NORMALLY_OPEN);
 		angleTalon.ConfigRevLimitSwitchNormallyOpen(REV_LIMIT_SWITCH_NORMALLY_OPEN);
 		angleTalon.enableLimitSwitch(true, true);//Now the limit switches are active.
-		//Because we can never trust limit switches, lets set some solf tops as well.
 	}
 	
 	@Override
@@ -45,21 +47,14 @@ public class AngleSubsystem extends Subsystem implements SensorListener {
 		this.setDefaultCommand(new ManualAngleCommand());
 	}
 	
-	//Shooter angle controls.
-	/**
-	 * Get the angle of the shooter.
-	 * @return An angle that the shooter is at. This will be in degrees. Range [-180, 180] though
-	 * 			The should be between [MIN_ANGLE, MAX_ANGLE] typically.
-	 */
-	
-	/**
-	 * Get the voltage measures from the talon.
-	 * @return The voltage measure in range [-1, 1]
-	 */
-	public double getAngleSpeed(){
-		//Set the speed of the motor that will change the angle.
-		return this.angleTalon.get();
-		}
+//	/**
+//	 * Get the voltage measures from the talon.
+//	 * @return The voltage measure in range [-1, 1]
+//	 */
+//	public double getAngleSpeed(){
+//		//Set the speed of the motor that will change the angle.
+//		return this.angleTalon.get();
+//		}
 	
 	/**
 	 * Set the speed this should move at.
@@ -69,6 +64,7 @@ public class AngleSubsystem extends Subsystem implements SensorListener {
 		//Set the speed of the motor that will change the angle.
 		this.angleTalon.set(speed);
 		}
+	
 	
 	//Did we hit a limit.
 	/**
@@ -97,7 +93,13 @@ public class AngleSubsystem extends Subsystem implements SensorListener {
 		return HumanInput.getXboxAxis(HumanInput.xboxController, HumanInput.XBOX_LEFT_STICK_Y_AXIS);
 	}
 	
-	//Angle managment.
+	@Override
+	/**
+	 * This sets the currentAngle based on current readings from sensor hub.
+	 * 
+	 * @param sensorMap A map of the sensors containing the sensor values to look at by look up by the 
+	 * 					two character key associated with each sensor.
+	 */
 	public void receivedValue(HashMap<String, Double> sensorMap) {
 		try{
 			currentAngle = sensorMap.get(Sensor.SHOOTER_ANGLE);
@@ -106,6 +108,13 @@ public class AngleSubsystem extends Subsystem implements SensorListener {
 		}
 	}
 	
+	/**
+	 * This gets the current cached angle value.
+	 * @return angle in degrees that was cached from the sensors earlier.
+	 * 			If the angle is not regularly given, give either 0 or the most recent value
+	 * 			however old that maybe. Range [-180, 180] though the should be between 
+	 * 			[MIN_ANGLE, MAX_ANGLE] typically.
+	 */
 	public double getCurrentAngle(){
 		return currentAngle;
 	}
