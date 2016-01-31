97873897d09f047b1f4ed5e623b431d51c680ea2
srgu2537 <matthewschweiss@verizon.net>
2016-01-31 09:39:47 -0500

    Updated the Triggers

--- Stronghold/src/org/usfirst/frc/team2537/robot/input/HumanInput.java
+++ Stronghold/src/org/usfirst/frc/team2537/robot/input/HumanInput.java
@@ -23,15 +23,69 @@ public class HumanInput {
 	public static Button raiseArm = new JoystickButton(xboxController, XboxButtons.XBOX_Y);
 	public static Button neutralArm = new JoystickButton(xboxController, XboxButtons.XBOX_B);
 	//Shooter things
-	public static final JoystickButton ballShootTrigger = new XboxTriggerRight(xboxController, XboxButtons.XBOX_TRIGGERS);
-
-	public static final JoystickButton harvestBallTrigger = new XboxTriggerLeft(xboxController, XboxButtons.XBOX_TRIGGERS);
+	public static final JoystickButton ballShootTrigger = new XboxTrigger(xboxController, XboxButtons.XBOX_RIGHT_TRIGGERS);
+	public static final JoystickButton harvestBallTrigger = new XboxTrigger(xboxController, XboxButtons.XBOX_LEFT_TRIGGERS);
 	
-	public static void registerPressedCommand(Button b, Command c) {
-		 b.whenPressed(c);
-	}
-	
+	/**
+	 * Get the value of the Xbox axis in question.
+	 * @param j The joystick to get axis measures from.
+	 * @param i The axis you want to get values for [0-6]
+	 * @return A double in the range [-1,1] in the amount the lever is depressed.
+	 */
 	public static double getXboxAxis(Joystick j, int i) {
 		return j.getRawAxis(i);
+	}
+	
+	/** 
+	 * Get a boolean value as to if one of the xbox triggers has been triggered.
+	 * 
+	 * @param j The joystick to get values on.
+	 * @param i The axis number to get values for. If i is not a trigger, but a joystick, 
+	 * 			any activation on the joystick will make this return true.
+	 * 
+	 * @return  If the trigger has been activated.
+	 */
+	public static boolean getXboxTrigger(Joystick j, int i){
+		return j.getRawAxis(i) != 0;
+	}
+		
+	/**
+	 * register a command for when the button is pressed
+	 * 
+	 * @param b
+	 *            the button to be registered found as a static Button in the
+	 *            HumanInput class.
+	 * @param cmd
+	 *            the user provided Command to be registered
+	 */
+	//Registers
+	public static void registerPressedCommand(Button b, Command cmd) {
+		 b.whenPressed(cmd);
+	}
+	
+//	/**
+//	 * register a command for when the button is released
+//	 * 
+//	 * @param b
+//	 *            the button to be registered found as a static Button in the
+//	 *            HumanInput class.
+//	 * @param cmd
+//	 *            the user provided Command to be registered
+//	 */
+//	public static void registerWhenReleasedCommand(Button b, Command cmd) {
+//		b.whenReleased(cmd);
+//	}
+
+	/**
+	 * register a command to be run as long as the button is held
+	 * 
+	 * @param b
+	 *            the button to be registered found as a static Button in the
+	 *            HumanInput class.
+	 * @param cmd
+	 *            the user provided Command to be registered
+	 */
+	public static void registerWhileHeldCommand(Button b, Command cmd) {
+		b.whileHeld(cmd);
 	}
 }
---
+++ Stronghold/src/org/usfirst/frc/team2537/robot/input/XboxTrigger.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.team2537.robot.input;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+public class XboxTrigger extends JoystickButton {
+	private int triggerTypeValue = 0;
+	
+	/**
+	 * 
+	 * @param joystick to go on always xbox.
+	 * @param triggerConstant which trigger to register pulled, from human input axis.
+	 */	
+	public XboxTrigger(GenericHID joystick, int triggerConstant) {
+		super(joystick, 0);
+		triggerTypeValue = triggerConstant;
+	} 
+	
+	@Override 
+	/**
+	 * @return trigger value
+	 */
+	public boolean get() {
+		
+		double triggerVal = HumanInput.getXboxAxis(HumanInput.xboxController, triggerTypeValue);
+		if (triggerVal >= 0.85) {
+			return true;
+		}
+		return false;
+	}
+
+}
