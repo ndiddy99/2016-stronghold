cefa6a482c4793917e53ac8cc7fad47c91819890
srgu2537 <matthewschweiss@verizon.net>
2016-01-31 19:48:24 -0500

    Sol fixed

--- Stronghold/src/org/usfirst/frc/team2537/robot/shooter/flywheel/SolenoidCommand.java
+++ Stronghold/src/org/usfirst/frc/team2537/robot/shooter/flywheel/SolenoidCommand.java
@@ -10,44 +10,55 @@ import java.time.Instant;
 public class SolenoidCommand extends Command {
 	//Constant
 	private static final boolean WAIT_TO_COMPLETE = true;
-	private static final long WAIT_TIME = 3100;//Milliseconds TODO find real time.
+	private static final double WAIT_TIME = 3.1;//Seconds TODO find real time.
 	//Varibles
 	private boolean extended;
-	private Instant start_time;
-	private Instant end_time;
+	private boolean waitToComplete;
+	private Instant endTime;
 	
 	public SolenoidCommand(boolean extended){
 		this(extended, WAIT_TO_COMPLETE);
 	}
+	
     public SolenoidCommand(boolean extended, boolean waitToComplete) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.shooterFlywheelSys);
     	this.extended = extended;
-    	start_time = Instant.now();
-    	end_time = start_time.plusMillis(WAIT_TIME);
+    	this.waitToComplete = waitToComplete;
     }
-
+    
+    @Override
     // Called just before this Command runs the first time
     protected void initialize() {
     	//Fire the solenoid
     	Robot.shooterFlywheelSys.setSolenoid(extended);
+    	if (waitToComplete){
+    		endTime = Instant.now().plusMillis((long) (WAIT_TIME * 1000));
+    	} else {
+    		//Don't wait.
+    		endTime = Instant.MIN;
+    	}
     }
-
+    
+    @Override
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	//What for the time.
     }
-
+    
+    @Override
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return Instant.now().isAfter(endTime);
     }
-
+    
+    @Override
     // Called once after isFinished returns true
     protected void end() {
     }
-
+    
+    @Override
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
