4a80257455a53038aa525b91702c2b149627e295
srgu2537 <matthewschweiss@verizon.net>
2016-02-04 20:13:38 -0500

    Made Angle Subsystem changes

---
+++ Stronghold/src/org/usfirst/frc/team2537/robot/shooter/actuator/PIDAngleSystem.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc.team2537.robot.shooter.actuator;
+
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+
+/**
+ *
+ */
+public class PIDAngleSystem extends PIDSubsystem {
+	private static final int DEFAULT_P = 0;
+	private static final int DEFAULT_I = 0;
+	private static final int DEFUALT_D = 0;
+
+    // Initialize your subsystem here
+    public PIDAngleSystem() {
+        // Use these to get going:
+        // setSetpoint() -  Sets where the PID controller should move the system
+        //                  to
+        // enable() - Enables the PID controller.
+    	super(0, 0, 0);
+    }
+    
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    protected double returnPIDInput() {
+        // Return your input value for the PID loop
+        // e.g. a sensor, like a potentiometer:
+        // yourPot.getAverageVoltage() / kYourMaxVoltage;
+    	return 0.0;
+    }
+    
+    protected void usePIDOutput(double output) {
+        // Use output to drive your system, like a motor
+        // e.g. yourMotor.set(output);
+    }
+}
