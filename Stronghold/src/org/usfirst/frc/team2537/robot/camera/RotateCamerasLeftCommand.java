8ef4071b3949e77aff15b9ed7b4542e69ea855a0
Space Raiders <Space Raiders@Team2537-Zenith.lan>
2016-02-23 15:08:50 -0500

    Dammit

--- Stronghold/src/org/usfirst/frc/team2537/robot/camera/RotateCamerasLeftCommand.java
+++ Stronghold/src/org/usfirst/frc/team2537/robot/camera/RotateCamerasLeftCommand.java
@@ -1,47 +1,47 @@
-package org.usfirst.frc.team2537.robot.camera;
-
-import java.util.Queue;
-
-import org.usfirst.frc.team2537.robot.Robot;
-
-import edu.wpi.first.wpilibj.command.Command;
-
-public class RotateCamerasLeftCommand extends Command {
-
-	@Override
-	protected void initialize() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected void execute() {
-		// TODO Auto-generated method stub
-		Queue<Integer> cameras = Robot.feeds.getCameras();
-		cameras.add(Robot.feeds.getCurCam());
-		cameras.add(cameras.poll());
-		int secondoff = cameras.poll();
-
-		Robot.feeds.setCurCam(secondoff);
-		Robot.feeds.changeCam(secondoff);
-	}
-
-	@Override
-	protected boolean isFinished() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
-	protected void end() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected void interrupted() {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+//package org.usfirst.frc.team2537.robot.camera;
+//
+//import java.util.Queue;
+//
+//import org.usfirst.frc.team2537.robot.Robot;
+//
+//import edu.wpi.first.wpilibj.command.Command;
+//
+//public class RotateCamerasLeftCommand extends Command {
+//
+//	@Override
+//	protected void initialize() {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	@Override
+//	protected void execute() {
+//		// TODO Auto-generated method stub
+//		Queue<Integer> cameras = Robot.feeds.getCameras();
+//		cameras.add(Robot.feeds.getCurCam());
+//		cameras.add(cameras.poll());
+//		int secondoff = cameras.poll();
+//
+//		Robot.feeds.setCurCam(secondoff);
+//		Robot.feeds.changeCam(secondoff);
+//	}
+//
+//	@Override
+//	protected boolean isFinished() {
+//		// TODO Auto-generated method stub
+//		return true;
+//	}
+//
+//	@Override
+//	protected void end() {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	@Override
+//	protected void interrupted() {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//}
